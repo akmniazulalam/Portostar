24aa10efd8214d12e7f916767b72a395c70e79be
niazulalam <niazulalam097@gmail.com>
2025-06-26 11:49:48 +0600

    CounterUp and Slider Add

--- src/components/layouts/Clients.jsx
+++ src/components/layouts/Clients.jsx
@@ -1,25 +1,75 @@
-import React from 'react'
-import Container from '../Container'
-import Flex from '../Flex'
-import Image from '../Image'
-import LogoOne from '../../assets/logoipsum-logo-1.png'
-import LogoTwo from '../../assets/logoipsum-logo-3.png'
-import LogoThree from '../../assets/logoipsum-logo-6.png'
-import LogoFour from '../../assets/logoipsum-logo-7.png'
+import Slider from "react-slick";
+import "../clients.css"
+import Container from "../Container";
+import Image from "../Image";
+import LogoOne from "../../assets/logo-6.png";
+import LogoTwo from "../../assets/logo-7.png";
+import LogoThree from "../../assets/logo-3.png";
+import LogoFour from "../../assets/logo-5.png";
+
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: { slidesToShow: 3 },
+    },
+    {
+      breakpoint: 768,
+      settings: { slidesToShow: 2 },
+    },
+    {
+      breakpoint: 480,
+      settings: { slidesToShow: 1 },
+    },
+    {
+      breakpoint: 320,
+      settings: { slidesToShow: 1 },
+    },
+  ],
+};
 
 const Clients = () => {
   return (
-    <section className='pt-10 pb-20'>
-        <Container>
-            <Flex className={"grid grid-cols-4 place-items-center"}>
-                <Image className={""} Src={LogoOne} Alt={LogoOne}/>
-                <Image className={""} Src={LogoTwo} Alt={LogoTwo}/>
-                <Image className={""} Src={LogoThree} Alt={LogoThree}/>
-                <Image className={""} Src={LogoFour} Alt={LogoFour}/>
-            </Flex>
-        </Container>
+    <section className="pt-10 pb-20">
+      <Container>
+        <div className="my-css">
+          <Slider {...settings}>
+          <div>
+            <Image className="" Src={LogoOne} Alt="LogoOne" />
+          </div>
+          <div>
+            <Image className="" Src={LogoTwo} Alt="LogoTwo" />
+          </div>
+          <div>
+            <Image className="" Src={LogoThree} Alt="LogoThree" />
+          </div>
+          <div>
+            <Image className="" Src={LogoFour} Alt="LogoFour" />
+          </div>
+          <div>
+            <Image className="" Src={LogoOne} Alt="LogoOne" />
+          </div>
+          <div>
+            <Image className="" Src={LogoTwo} Alt="LogoTwo" />
+          </div>
+          <div>
+            <Image className="" Src={LogoThree} Alt="LogoThree" />
+          </div>
+          <div>
+            <Image className="" Src={LogoFour} Alt="LogoFour" />
+          </div>
+        </Slider>
+        </div>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Clients
+export default Clients;
--- src/components/layouts/Experience.jsx
+++ src/components/layouts/Experience.jsx
@@ -3,7 +3,7 @@ import Container from "../Container";
 import Flex from "./../Flex";
 import ExperienceH from "../ExperienceH";
 import Texts from "../Texts";
-
+import CountUp from 'react-countup'
 
 
 const Experience = () => {
@@ -13,19 +13,19 @@ const Experience = () => {
         <div className="text-center">
           <Flex className={"grid grid-cols-4"}>
             <div className="">
-              <ExperienceH className={""} experienceHeading={"100+"} />
+              <ExperienceH className={""} experienceHeading={<CountUp start={0} end={100} duration={1.5} enableScrollSpy suffix="+"/>} />
               <Texts className={"font-medium uppercase pt-2"} pText={"Projects"}/>
             </div>
             <div className="">
-              <ExperienceH className={""} experienceHeading={"24"} />
+              <ExperienceH className={""} experienceHeading={<CountUp start={0} end={24} duration={1.5} enableScrollSpy/>} />
               <Texts className={"font-medium uppercase pt-2"} pText={"WINNING AWARD"}/>
             </div>
             <div className="">
-              <ExperienceH className={""} experienceHeading={"70+"} />
+              <ExperienceH className={""} experienceHeading={<><CountUp start={0} end={70} duration={1.5} enableScrollSpy/>+</>} />
               <Texts className={"font-medium uppercase pt-2"} pText={"HAPPY CLIENTS"}/>
             </div>
             <div className="">
-              <ExperienceH className={""} experienceHeading={"10"} />
+              <ExperienceH className={""} experienceHeading={<CountUp start={0} end={10} duration={1.5} enableScrollSpy/>} />
               <Texts className={"font-medium uppercase pt-2"} pText={"YEAR EXPERIENCE"}/>
             </div>
           </Flex>
